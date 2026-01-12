16a998a112ad6e7ffbe6ed78b0d0655f063afb2e
BlockOG <me@blockog.net>
2026-01-12 17:29:42 +0200

    add vkcapture plugin to obs

--- modules/obs/nixos.nix
+++ modules/obs/nixos.nix
@@ -1,4 +1,5 @@
-{
+{pkgs, ...}: {
   programs.obs-studio.enable = true;
   programs.obs-studio.enableVirtualCamera = true;
+  programs.obs-studio.plugins = with pkgs; [obs-studio-plugins.obs-vkcapture];
 }
