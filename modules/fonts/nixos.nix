9469b715929c64dc379eeb723ab87ab4610871e2
BlockOG <me@blockog.net>
2026-02-12 21:27:31 +0200

    add a cjk font

--- modules/fonts/nixos.nix
+++ modules/fonts/nixos.nix
@@ -3,8 +3,10 @@
     nerd-fonts.jetbrains-mono
     roboto
     roboto-serif
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
   ];
   fonts.fontconfig.defaultFonts.monospace = ["JetBrainsMono NFM"];
-  fonts.fontconfig.defaultFonts.serif = ["Roboto Serif"];
-  fonts.fontconfig.defaultFonts.sansSerif = ["Roboto"];
+  fonts.fontconfig.defaultFonts.serif = ["Roboto Serif" "Noto CJK Serif"];
+  fonts.fontconfig.defaultFonts.sansSerif = ["Roboto" "Noto CJK Sans"];
 }
